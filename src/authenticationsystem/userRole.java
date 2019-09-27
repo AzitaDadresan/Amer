b638a138083d1d60786d0b4e45fe5d1a495a65ba
Azidad <a_dadresan@yahoo.com>
2019-09-26 18:58:34 -0700

    AuthenticationSystem

---
+++ src/authenticationsystem/userRole.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package authenticationsystem;
+
+/**
+ *
+ * @author Azita Dadresan
+ */
+
+
+class userRole {
+    public String getUserRole(){
+        String userRole = null;
+        return userRole;
+    }
+
+    String setUser(String username) {
+        return username;
+    }
+
+    String printWelcome() {
+        return "Welcome";
+    }
+}
